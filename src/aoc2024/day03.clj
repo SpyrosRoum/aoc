e5969117f9af33a5b9c1d1a9ef2aea560a19c90f
Spyros Roum <spyros.roum@posteo.net>
2024-12-03 10:57:29 +0200

    Day 3 but even better

--- src/aoc2024/day03.clj
+++ src/aoc2024/day03.clj
@@ -1,41 +1,33 @@
 (ns aoc2024.day03
-  (:require [clojure.string :refer [join]]
-             [clojure.tools.trace :refer [trace]]))
+  (:require
+   [clojure.string :refer [join]]))
 
 (def example-text "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
 (def example-text2 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+(def raw-text (slurp "inputs/day03"))
 
 (def mul-regex #"mul\((\d{1,3}),(\d{1,3})\)")
 (def do-dont-regex #"do(?:n't)?\(\)")
 (def either-regex (re-pattern (join "|" [mul-regex do-dont-regex])))
 
-(def raw-text (slurp "inputs/day03"))
-
 (defn part1
   [text]
   (reduce
-   +
-   0
-   (for [[_ a b] (re-seq mul-regex text)]
-     (* (parse-long a) (parse-long b))
-     )))
+    +
+    0
+    (for [[_ a b] (re-seq mul-regex text)]
+      (* (parse-long a) (parse-long b)))))
 
-(defn part2-multiplier
-  ([col] (part2-multiplier col true 0))
-  ([col should_multiply total]
+(defn part2
+  ([text]
+   (part2 (re-seq either-regex text) true 0))
+  ([col should-multiply total]
    (if (empty? col)
      total
      (let [[[action a b] & tail] col]
-       (if should_multiply
-         (if (and (some? a) (some? b))
-           ;; We have a multiplication, and we should multiply
-           (recur tail should_multiply (+ total (* (parse-long a) (parse-long b))))
-           ;; We have a do or dont
-           (recur tail (= action "do()") total))
-         ;; We shouldn't multiply, no need to check what exactly we have
+       (if (and should-multiply (re-matches #"mul.*" action))
+         (recur tail should-multiply (+ total (* (parse-long a) (parse-long b))))
          (recur tail (= action "do()") total))))))
 
-
-(defn part2
-  [text]
-  (part2-multiplier (re-seq either-regex text)))
+(part1 raw-text)
+(part2 raw-text)
