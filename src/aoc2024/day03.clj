908631abbdd1b7b91c1e1658b6a47b5bd849d8f7
Spyros Roum <spyros.roum@posteo.net>
2024-12-03 10:46:26 +0200

    Day 3 no atom

--- src/aoc2024/day03.clj
+++ src/aoc2024/day03.clj
@@ -1,5 +1,6 @@
 (ns aoc2024.day03
-  (:require [clojure.string :refer [join]]))
+  (:require [clojure.string :refer [join]]
+             [clojure.tools.trace :refer [trace]]))
 
 (def example-text "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
 (def example-text2 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
@@ -20,18 +21,21 @@
      )))
 
 (defn part2-multiplier
-  [text]
-  (let [should_mul (atom true)]
-    (for [[action a b] (re-seq either-regex text)]
-      (if (and (some? a) (some? b))
-        (if (deref should_mul)
-          (let [a (parse-long a)
-                b (parse-long b)]
-            (* a b)))
-        (if (= action "do()")
-          (reset! should_mul true)
-          (reset! should_mul false))))))
+  ([col] (part2-multiplier col true 0))
+  ([col should_multiply total]
+   (if (empty? col)
+     total
+     (let [[[action a b] & tail] col]
+       (if should_multiply
+         (if (and (some? a) (some? b))
+           ;; We have a multiplication, and we should multiply
+           (recur tail should_multiply (+ total (* (parse-long a) (parse-long b))))
+           ;; We have a do or dont
+           (recur tail (= action "do()") total))
+         ;; We shouldn't multiply, no need to check what exactly we have
+         (recur tail (= action "do()") total))))))
+
 
 (defn part2
   [text]
-  (reduce + (filter number? (part2-multiplier text))))
+  (part2-multiplier (re-seq either-regex text)))
